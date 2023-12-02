ea5bf3290484266e0f178ccc2b124d248d54f3f9
FemixGH <fedor_a95@mail.ru>
2023-12-03 02:08:06 +0300

    осталось страницу о нас

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,40 @@ document.body.addEventListener('click', event => {
     // Действие при клике
     document.querySelector(".header").classList.remove("open")
 });
+
+
+let intervalId;
+
+const handleImageChange = (offset) => {
+    const activeSlide = document.querySelector("[data-active]");
+    const slides = [...document.querySelectorAll(".slide")];
+    const currentIndex = slides.indexOf(activeSlide);
+    let newIndex = currentIndex + offset;
+
+    if (newIndex < 0) newIndex = slides.length - 1;
+    if (newIndex >= slides.length) newIndex = 0;
+
+    slides[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+};
+
+const onNext = () => {
+    handleImageChange(1);
+    resetInterval();
+};
+
+const onPrev = () => {
+    handleImageChange(-1);
+    resetInterval();
+};
+
+const startInterval = () => {
+    intervalId = setInterval(onNext, 10000); // 10000 миллисекунд = 10 секунд
+};
+
+const resetInterval = () => {
+    clearInterval(intervalId);
+    startInterval();
+};
+
+startInterval(); // запускаем автоматическое переключение при загрузке страницы
